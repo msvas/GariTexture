0fb064ebc5d369137b9ae9f32b37f115e45e87a6
Marcelo Vasques <msouvas@gmail.com>
2016-09-19 14:57:50 -0300

    Bent tube and speed fixed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-    [Range(0, 1)]
-    public float speed = 0.5f;
+    [Range(1, 10)]
+    public float speed = 1f;
 
     private Vector3 pos = new Vector3(0, 0, 0);
 
@@ -15,7 +15,7 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        pos.z += speed * Time.deltaTime;
+        pos.z += speed / 1000 * Time.deltaTime;
         transform.Translate(pos);
 	}
 }
