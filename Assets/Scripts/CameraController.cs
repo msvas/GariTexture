67a05526a3859989afa22a7264fc40c02c54ac90
Marcelo Vasques <msouvas@gmail.com>
2016-09-22 14:41:22 -0300

    Camera comes back to original position. Real image now used.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,16 +6,22 @@ public class CameraController : MonoBehaviour {
     [Range(1, 10)]
     public float speed = 1f;
 
+    private int tubeSize;
+
     private Vector3 pos = new Vector3(0, 0, 0);
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        tubeSize = GameObject.FindGameObjectWithTag("TubeCreator").GetComponent<TubeCreator>().tubeSize;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        pos.z += speed / 1000 * Time.deltaTime;
-        transform.Translate(pos);
+        if (gameObject.transform.position.z < tubeSize) {
+            pos.z += speed / 1000 * Time.deltaTime;
+            transform.Translate(pos);
+        } else {
+            transform.Translate(0, 0, -tubeSize);
+        }
 	}
 }
